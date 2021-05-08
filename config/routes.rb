996cfe18f6e0f90289f5c87f7867b006ae50766d
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-08 02:14:50 +0000

    [Start]phase2-4-pra

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
+  
   resources :books
 
   root to: 'homes#top'
