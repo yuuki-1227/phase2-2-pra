36d9385956e236b73a927b7eb5255d555c6416cd
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-10 09:20:44 +0000

    [Update]コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,2 +1,24 @@
 class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(book_id: @book.id, user_id: current_user.id)
+    @book_comment.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def  book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :books do
     resource :favorites, only: [:create, :destroy]
-    resource :book_comments, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
 
   root to: 'homes#top'
